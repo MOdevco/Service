ee3128d886f69b0b93252e35073b6359eb423118
codewizrad <codewiizard@gmail.com>
2023-09-22 05:26:54 -0700

    all

--- src/components/tables/tables.jsx
+++ src/components/tables/tables.jsx
@@ -1,7 +1,7 @@
 import { Box, Center, Flex, Text } from "@chakra-ui/layout";
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineMoreVert } from "react-icons/md";
-import { AiFillPlusCircle } from "react-icons/ai";
+import { AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 import {
   Table,
   Thead,
@@ -10,15 +10,60 @@ import {
   Th,
   Td,
   TableContainer,
+  Button,
+  Input,
 } from "@chakra-ui/react";
 
 const Tables = ({ title }) => {
+  const [open, setopen] = useState(true);
+  const handleClick = () => setopen(!open)
   return (
     <Box>
       <Box display={"Flex"} alignItems={"center"} gap="10px" mt={"35px"}>
         <Text>{title}</Text>
         <Button bg={'transparent'} _hover={''} _active={''} onClick={handleClick}>{open ? <AiFillMinusCircle color="#B10202" /> : <AiFillPlusCircle color="#4CAF50" />}</Button>
       </Box>
+      {open && <Box pb={"25px"} display={"flex"} alignItems={"center"}>
+        <Input
+          width={"20%"}
+          placeholder="Ventilyator"
+          h={"2.4rem"}
+          size="md"
+          borderRightRadius={"none"}
+        />
+        <Button
+          bg={"#4CAF50"}
+          color={"#fff"}
+          size="md"
+          borderRadius={"3px"}
+          _hover={"none"}
+          _active={"none"}
+        >
+          Qo’shish
+        </Button>
+        <Box pl={"15px"} display={'flex'} alignItems={'center'} gap={'15px'}>
+          <Button
+            _hover={"none"}
+            color={"#fff"}
+            padding={"11px 31px"}
+            _active={"none"}
+            bg={"#404E67"}
+            borderRadius={'3px'}
+          >
+            Excel
+          </Button>
+          <Button
+            _hover={"none"}
+            color={"#fff"}
+            padding={"11px 31px"}
+            _active={"none"}
+            bg={"#3A69BB"}
+            borderRadius={'3px'}
+          >
+            Yuklash
+          </Button>
+        </Box>
+      </Box>}
 
       <TableContainer shadow={"0px 2px 8px 0px rgba(0, 0, 0, 0.12)"}>
         <Table width={"100%"} rounded={"16px"} fontSize={'19px'}>
