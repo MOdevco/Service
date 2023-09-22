d090a284eeb5ed7daca769523d3bfc2fd58f50bb
abdulazizxoldorov <abdulazizxoldorov05@gmail.com>
2023-09-22 12:15:57 +0500

    navbar teyyar

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import coins from './coins.png'
+import recive from './recive.png'
+import report from './report.png'
+import coin from './coin.png'
+export {coins,recive,report,coin}
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,20 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import React from "react";
+import NavbarLinks from "../navbarLinks/NavbarLinks";
+
+function Navbar() {
+  return (
+    <Box>
+      <Box bg={"#404E67"} pl={'26px'} pt={'16px'} pb={"17px"} width={"100%"}>
+        <Box display={'flex'} alignItems={'center'} gap={'50px'}>
+          <Heading fontSize={"36px"} fontWeight={"500"} color={"#fff"}>
+            iStore
+          </Heading>
+          <NavbarLinks />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Navbar;
