bf5995088f1367ac099792ad0afe04ebe1b90f60
akmaljon010 <akmaljonummataliyev0@gmail.com>
2023-09-22 10:37:00 +0500

    uikukkl

--- src/components/asosiybody/asosiybody.jsx
+++ src/components/asosiybody/asosiybody.jsx
@@ -1,10 +1,20 @@
 import { Box } from '@chakra-ui/layout'
 import React from 'react'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 
 const Asosiybody = () => {
   return (
     <Box>
-        adasf
     </Box>
   )
 }
