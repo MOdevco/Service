9291cc982c362442ba0399bcde85c1ed23429386
Hikmatillo019 <rayimjonovhikmatillo3@gmail.com>
2023-09-22 10:32:24 +0500

    asdas

---
+++ src/components/asosiybody/asosiybody.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/layout'
+import React from 'react'
+
+const Asosiybody = () => {
+  return (
+    <Box>
+        adasf
+    </Box>
+  )
+}
+
+export default Asosiybody
