7653fb80af0e2739a164b82bea5e4570be2d3bf8
Hikmatillo019 <rayimjonovhikmatillo3@gmail.com>
2023-09-22 12:17:17 +0500

    fsd

--- src/components/asosiybody/asosiybody.jsx
+++ src/components/asosiybody/asosiybody.jsx
@@ -1,12 +1,65 @@
-import { Box } from '@chakra-ui/layout'
-import React from 'react'
+import { Box, Center, Flex, Text } from "@chakra-ui/layout";
+import React from "react";
+import {MdOutlineMoreVert } from 'react-icons/md'
+import {AiFillPlusCircle} from  'react-icons/ai'
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
 
 const Asosiybody = () => {
   return (
-    <Box>
-        adasf
+    <Box w={'1362px'} m={' auto'} >
+      <TableContainer w={'1419px'} m={'0 50px'}>
+        <Box display={'Flex'} alignItems={'center'} gap="10px" mt={'35px'}>
+            <Text>Tovar sotiladigan qurilmalar</Text>
+            <AiFillPlusCircle />
+        </Box>
+        <Table width={'100%'} rounded={'16px'} >
+            <Thead>
+                <Tr bg='#F1F3F9'>
+                    <Th>№</Th>
+                    <Th> Nomi</Th>
+                    <Th>Qo’shilgan sana</Th>
+                    <Th >User</Th>
+                    <Th></Th>
+                </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>1</Td>
+              <Td>Mobile maxsulotlar</Td>
+              <Td>22 Noy 2023</Td>
+              <Td>Komiljon Soliyev Xaydarovich</Td>
+              <Td><MdOutlineMoreVert size={'29px'} /></Td>
+            </Tr>
+            <Tr>
+              <Td>1</Td>
+              <Td>Mobile maxsulotlar</Td>
+              <Td>22 Noy 2023</Td>
+              <Td>Komiljon Soliyev Xaydarovich</Td>
+              <Td><MdOutlineMoreVert size={'29px'} /></Td>
+            </Tr>
+            <Tr>
+              <Td>1</Td>
+              <Td>Mobile maxsulotlar</Td>
+              <Td>22 Noy 2023</Td>
+              <Td>Komiljon Soliyev Xaydarovich</Td>
+              <Td><MdOutlineMoreVert size={'29px'} /></Td>
+            </Tr>
+            
+            
+          </Tbody>
+          
+        </Table>
+      </TableContainer>
     </Box>
-  )
-}
+  );
+};
 
-export default Asosiybody
+export default Asosiybody;
