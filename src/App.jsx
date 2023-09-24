9ca3f49a42fc8b91dbc102db7878cc3a39f24375
abdulazizxoldorov <abdulazizxoldorov05@gmail.com>
2023-09-24 21:38:50 +0500

    all

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,6 @@ import AsosiyMalumatlar from "./pages/AsosiyMalumotlar";
 import Asosiybody from "./components/asosiybody/asosiybody";
 import Navbar from "./components/navbar/navbar";
 import Login from "./pages/Login";
-import TableSotuv from "./components/tableSotuv/tableSotuv";
-import TavarCatigory from "./components/tavarCatigory/tavarCatigory";
-import TavarCatigoryTab from "./components/tavarCatigoryTab/tavarCatigoryTab";
 
 function App() {
   return (
@@ -23,75 +20,7 @@ function App() {
       <Box>
         <Navbar />
       </Box>
-      <Tabs position="relative">
-        <TabList display={"flex"} gap={"30px"} pt={"20px"} pl={"60px"} borderBottom={'none'}>
-          <Tab
-            fontSize={"20px"}
-            fontWeight={"500"}
-            lineHeight={"20px"}
-            wordBreak={"break-word"}
-            _selected={{ color: '#FF5C00'}}
-          >
-            Tovar sotiladigan qurilmalar
-          </Tab>
-          <Tab
-            fontSize={"20px"}
-            fontWeight={"500"}
-            lineHeight={"20px"}
-            wordBreak={"break-word"}
-            _selected={{ color: '#FF5C00'}}
-          >
-            Tovar kategoriyalar
-          </Tab>
-          <Tab
-            fontSize={"20px"}
-            fontWeight={"500"}
-            lineHeight={"20px"}
-            wordBreak={"break-word"}
-            _selected={{ color: '#FF5C00'}}
-          >
-            Tovar turlari
-          </Tab>
-          <Tab
-            fontSize={"20px"}
-            fontWeight={"500"}
-            lineHeight={"20px"}
-            wordBreak={"break-word"}
-            _selected={{ color: '#FF5C00'}}
-          >
-            Kategoriya turlar
-          </Tab>
-          <Tab
-            fontSize={"20px"}
-            fontWeight={"500"}
-            lineHeight={"20px"}
-            wordBreak={"break-word"}
-            _selected={{ color: '#FF5C00'}}
-          >
-            Sotuv miqdor turlari
-          </Tab>
-          <Tab
-            fontSize={"20px"}
-            fontWeight={"500"}
-            lineHeight={"20px"}
-            wordBreak={"break-word"}
-            _selected={{ color: '#FF5C00'}}
-          >
-            Tovar firmalari
-          </Tab>
-        </TabList>
-        <TabIndicator
-          mt="-1.5px"
-          height="2px"
-          bg="blue.500"
-          borderRadius="1px"
-        />
-        <TabPanels>
-          <TabPanel  maxWidth={"1832px"} m={"auto"}> <TableSotuv /> </TabPanel>
-          <TabPanel maxWidth={"1832px"} m={"auto"}> <TavarCatigory /> </TabPanel>
-          <TabPanel maxWidth={"1832px"} m={"auto"}> <TavarCatigoryTab /></TabPanel>
-        </TabPanels>
-      </Tabs>
+
       <Box maxWidth={"1832px"} m={"auto"}>
         <Routes>
           <Route path="/" element={<AsosiyMalumatlar />}></Route>
--- src/components/asosiybody/asosiybody.jsx
+++ src/components/asosiybody/asosiybody.jsx
@@ -1,30 +1,105 @@
 import React from "react";
 
-
 import TableSotuv from "../tableSotuv/tableSotuv";
 import TavarCatigoryTab from "../tavarCatigoryTab/tavarCatigoryTab";
 import TavarCatigory from "../tavarCatigory/tavarCatigory";
 import TavarCatigoryEnd from "../tavarCatigoryEnd/tavarCatigoryEnd";
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Tab,
+  TabIndicator,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 
 const Asosiybody = () => {
-
-
-  
   return (
-    <Box p={'50px'}>
+    <Box p={"50px"}>
       {/* birinvhi table */}
-     <TableSotuv />
+      <Tabs position="relative" variant="">
+        <TabList>
+          <Tab
+            fontSize={"20px"}
+            fontWeight={"500"}
+            lineHeight={"20px"}
+            wordBreak={"break-word"}
+            _selected={{ color: "#FF5C00" }}
+          >
+            Tovar sotiladigan qurilmalar
+          </Tab>
+          <Tab
+            fontSize={"20px"}
+            fontWeight={"500"}
+            lineHeight={"20px"}
+            wordBreak={"break-word"}
+            _selected={{ color: "#FF5C00" }}
+          >
+            Tovar kategoriyalar
+          </Tab>
+          <Tab
+            fontSize={"20px"}
+            fontWeight={"500"}
+            lineHeight={"20px"}
+            wordBreak={"break-word"}
+            _selected={{ color: "#FF5C00" }}
+          >
+            Tovar turlari
+          </Tab>
+          <Tab
+            fontSize={"20px"}
+            fontWeight={"500"}
+            lineHeight={"20px"}
+            wordBreak={"break-word"}
+            _selected={{ color: "#FF5C00" }}
+          >
+            Kategoriya turlar
+          </Tab>
+          <Tab
+            fontSize={"20px"}
+            fontWeight={"500"}
+            lineHeight={"20px"}
+            wordBreak={"break-word"}
+            _selected={{ color: "#FF5C00" }}
+          >
+            Sotuv miqdor turlari
+          </Tab>
+          <Tab
+            fontSize={"20px"}
+            fontWeight={"500"}
+            lineHeight={"20px"}
+            wordBreak={"break-word"}
+            _selected={{ color: "#FF5C00" }}
+          >
+            Tovar firmalari
+          </Tab>
+        </TabList>
+        <TabIndicator
+          mt="-1.5px"
+          height="2px"
+          bg="blue.500"
+          borderRadius="1px"
+        />
+        <TabPanels>
+          <TabPanel maxWidth={"1832px"} m={"auto"}>
+            {" "}
+            <TableSotuv />{" "}
+          </TabPanel>
+          <TabPanel maxWidth={"1832px"} m={"auto"}>
+            {" "}
+            <TavarCatigory />{" "}
+          </TabPanel>
+          <TabPanel maxWidth={"1832px"} m={"auto"}>
+            {" "}
+            <TavarCatigoryTab />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
 
-
-    <TavarCatigoryTab />
-    
-
-    <TavarCatigory />
-
-     {/* <TavarCatigoryEnd /> */}
+      {/* <TavarCatigoryEnd /> */}
     </Box>
   );
-}
+};
 
-export default Asosiybody
+export default Asosiybody;
